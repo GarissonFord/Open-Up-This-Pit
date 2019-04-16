c5b45b4193b8c9f088ce01a747590e6c726615f2
Gary DiCiano <garydiciano4@gmail.com>
2019-04-16 11:20:24 -0400

    Player Replaced
    The player is now given a first person view

---
+++ Open-Up-This-Pit/Assets/DroneController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneController : MonoBehaviour
+{
+    Rigidbody rb;
+    public float h, v;
+    public float speed;
+
+    public float mouseX, mouseY;
+    float heading, heading2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = -Input.GetAxis("Mouse Y");
+
+        //Vector3 movement = new Vector3(h, 0.0f, v);
+        Vector3 movement = transform.right * h + transform.forward * v;
+        rb.velocity = movement * speed;
+
+        //Apply the X rotation
+        heading += (mouseX) * Time.deltaTime * 180;
+        heading2 += (mouseY) * Time.deltaTime * 180;
+
+        transform.rotation = Quaternion.Euler(heading2, heading, 0);
+    }
+}
--- Open-Up-This-Pit/Assets/Scripts/PlayerController.cs
+++ Open-Up-This-Pit/Assets/Scripts/PlayerController.cs
@@ -18,6 +18,13 @@ public class PlayerController : MonoBehaviour
     //Direct reference to the camera
     public Transform cam;
 
+    //Movement input
+    Vector2 input;
+
+    public Vector3 currentRotation;
+
+    public float h, v;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +32,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
+    /*
     void Update()
     {
         float h = Input.GetAxis("Horizontal");
@@ -35,12 +43,74 @@ public class PlayerController : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y");
 
         //Apply the X rotation
-        //Using either mouseX or inputX 
         heading += (mouseX) * Time.deltaTime * 180;
         heading2 += (mouseY) * Time.deltaTime * 180;
 
+        camPivot.rotation = Quaternion.Euler(heading2, heading, 0);
+
+
         Vector3 movement = new Vector3(h, 0.0f, v);
         rb.velocity = movement * speed;
+    }
+    */
+
+    void Update()
+    {
+        //Mouse input
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        //Stick input
+        float inputX = Input.GetAxis("Horizontal"), inputZ = Input.GetAxis("Vertical");
+        //Apply the X rotation
+        //Using either mouseX or inputX 
+        heading += (mouseX + inputX) * Time.deltaTime * 180;
+        heading2 += (mouseY + inputZ) * Time.deltaTime * 180;
+        camPivot.rotation = Quaternion.Euler(heading2, heading, 0);
+
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        //If the player is moving at all
+        /*
+        if (h != 0 || v != 0)
+            anim.SetBool("IsMoving", true);
+        else if (h == 0 && v == 0)
+            anim.SetBool("IsMoving", false);
+            */
+
+        Vector3 movement = new Vector3(inputX, 0.0f, inputZ);
+
+        input = new Vector2(h, v);
+        input = Vector2.ClampMagnitude(input, 1);
+
+        Vector3 camF = cam.forward; //forward
+        Vector3 camR = cam.right; //right
+
+        //Because of the downward angle the camera is facing
+        camF.y = 0;
+        camR.y = 0;
+        camF = camF.normalized;
+        camR = camR.normalized;
+
+        // v > 0 Means the player's input is to move forward
+
+        if (v != 0)
+        {
+            //Sets rotation to the camera pivot's forward
+            currentRotation = camPivot.eulerAngles;
+            transform.eulerAngles = currentRotation;
+        }
+
+        //Walking backwards
+        /*
+        if (v < 0)
+            anim.SetBool("MovingBackwards", true);
+        else if (v >= 0)
+            anim.SetBool("MovingBackwards", false);
+            */
+
+        //Final movement update
+        transform.position += (camF * input.y + camR * input.x) * Time.deltaTime * 5;
     }
 
     void FixedUpdate()
